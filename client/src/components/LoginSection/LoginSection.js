cb0962ff74aa36b4ff69b80da31ce5d4efc177a6
Guillermo Garcia <ggq0107@hotmail.com>
2021-05-26 20:58:24 -0400

    quick fixes

--- client/src/components/LoginSection/LoginSection.js
+++ client/src/components/LoginSection/LoginSection.js
@@ -11,64 +11,33 @@ import { Link } from 'react-router-dom';
 
 function LoginSection() {
 
-    return (
+  return (
 
-        <div className = 'hero-container'>
-            <img src = '/images/landscaping3.jpg' alt ="landscaping"></img>
-            <h1> </h1>
+    <div className='hero-container'>
+      <img src='/images/landscaping3.jpg' alt="landscaping"></img>
+      <h1> </h1>
       < div className='hero-btns'>
         <ButtonHome
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-         Homeowners Login
+          Homeowners Login
         </ButtonHome>
         <ButtonLand
           className='btns'
           buttonStyle='btn--outline'
           buttonSize='btn--large'
         >
-         Landscapers Login
+          Landscapers Login
         </ButtonLand>
-        </div>    
-      //   <div className = 'hero-container'>
-      //       <img src = '/images/landscaping3.jpg' alt ="landscaping"></img>
-      //       <h1> </h1>
-      // < div className='hero-btns'>
-      //   <Button
-      //     className='btns'
-      //     buttonStyle='btn--outline'
-      //     buttonSize='btn--large'
-      //   >
-      //    Homeowners Login
-      //   </Button>
-      //   <Button
-      //     className='btns'
-      //     buttonStyle='btn--outline'
-      //     buttonSize='btn--large'
-      //   >
-      //    Landscapers Login
-      //   </Button>
-      //   </div>    
-      //   </div>
-      <section class="hero is-danger is-fullheight" id="hero-signup">
-      <div class="hero-body">
-        <div class="btn-container">
-          <Link to="/sign-up-homeowner">
-            <button class="button is-black">Homeowner Login</button>
-          </Link>
-          <Link to="sign-up-landscaper">
-          <button class="button is-black" id="btn-right">Landscaper Login</button>
-          </Link>
-        </div>
       </div>
-    </section>
+    </div>
 
 
 
 
-    );
+  );
 }
 
 export default LoginSection;
